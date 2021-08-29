b43f952cb5cc8a19ef3a5f52a8f5e3fbec635826
amin foroutan <aminforoutan7@gmail.com>
2021-08-29 13:05:31 +0430

    [feat]: implemented AVL tree

--- algorithms/AVL.py
+++ algorithms/AVL.py
@@ -2,22 +2,29 @@ from BST import BST, BSTNode
 
 
 class AVL(BST):
-    @staticmethod
-    def _fix_avl(node):
-        if node.get_balance() > 1:
-            right_balance = node.get_right_child().get_balance()
-            if right_balance == 1 or right_balance == 0:
-                node.left_rotation()
-            if right_balance == -1:
-                node.get_right_child().right_rotation()
-                node.left_rotation()
-        if node.get_balance < -1:
-            left_balance = node.get_right_child().get_balance()
-            if left_balance == -1 or left_balance == 0:
-                node.right_rotation()
-            if left_balance == 1:
-                node.get_left_child().left_rotation()
-                node.right_rotation()
+    def _fix_avl(self, node):
+        temp = node
+        while temp is not None:
+            if temp.get_balance() > 1:
+                if self.is_root(temp):
+                    self.root = temp.get_right_child()
+                right_balance = temp.get_right_child().get_balance()
+                if right_balance == 1 or right_balance == 0:
+                    temp.left_rotate()
+                if right_balance == -1:
+                    temp.get_right_child().right_rotate()
+                    temp.left_rotate()
+            if temp.get_balance() < -1:
+                if self.is_root(temp):
+                    self.root = temp.get_left_child()
+                left_balance = temp.get_left_child().get_balance()
+                if left_balance == -1 or left_balance == 0:
+                    temp.right_rotate()
+                if left_balance == 1:
+                    temp.get_left_child().left_rotate()
+                    temp.right_rotate()
+
+            temp = temp.parent
 
     def insert(self, data):
         if self.root:
@@ -40,6 +47,19 @@ class AVL(BST):
                         new_node.set_parent(node)
                         break
             BST.update_heights(new_node)
-            AVL._fix_avl(new_node)
+            self._fix_avl(new_node)
         else:
             self.root = BSTNode(data)
+
+
+if __name__ == '__main__':
+    avl = AVL()
+    avl.insert(38)
+    avl.insert(54)
+    avl.insert(55)
+    avl.insert(56)
+    avl.insert(57)
+    avl.insert(58)
+    avl.insert(59)
+    avl.insert(60)
+    avl.display()
